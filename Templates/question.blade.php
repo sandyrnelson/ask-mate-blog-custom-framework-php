6c3b474abdb65e2fd0844f05db43204ab02e1551
sandyrnelson <sandyrnelson@berkeley.edu>
2020-12-02 12:36:47 +0100

    Add link to delete question

--- Templates/question.blade.php
+++ Templates/question.blade.php
@@ -46,7 +46,9 @@
     <a href="/list">Questions</a>
     <a href="/tags">Tags</a>
 {{--    {% if session['username'] %}--}}
-    <a href="/ask-question">New Question</a><a href="/question/{{ $question['id']}}/edit">Edit the Question</a>
+    <a href="/ask-question">New Question</a>
+    <a href="/question/{{ $question['id']}}/edit">Edit the Question</a>
+    <a href="/question/{{ $question['id']}}/delete">Delete Question</a>
 {{--    {% endif %}--}}
     <div class="search-container">
         <form action="/search">
