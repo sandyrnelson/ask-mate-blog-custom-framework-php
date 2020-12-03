e11b6c3c2310208f1dfdcceb420b7689889bdb5f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-03 09:50:48 +0100

    Small refactor

--- App/Routes/GetRoutes.php
+++ App/Routes/GetRoutes.php
@@ -56,22 +56,5 @@ class GetRoutes
         }, "POST");
 
 
-
-//        Router::add("/get/([0-9]*)", function ($param) {
-//            echo "Got number $param";
-//        }, "GET");
-//
-//        Router::add("/get/([^0-9]*)", function ($param) {
-//            echo "Got string $param";
-//        }, "GET");
-//
-//        Router::add("/get/(.*)", function ($param) {
-//            echo "Got mixed $param";
-//        }, "GET");
-//
-//        Router::add("/get/(.*)/(.*)", function ($paramOne, $paramTwo) {
-//            echo "Got params $paramOne and $paramTwo";
-//        }, "GET");
-
     }
 }
